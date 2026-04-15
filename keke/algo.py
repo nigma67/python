daa839020904267f22577c52a372ccaf69c33ead
Alumno <alumno@et26.local>
2026-04-15 15:29:19 -0300

    kiara15-4

---
+++ keke/algo.py
@@ -0,0 +1,32 @@
+def eje1():
+    n1 = int(input("Ingresar un numero"))
+    n2 = int(input("Ingresar otro numero"))
+    R= n1+n2
+    print("El resultado es: ", R)
+    #eje1()
+
+def eje2():
+    arr=[]
+    for i in range(4):
+        arr.append(int(input("Ingresar un numero: "))
+                   print("El resultado es: ", sum(arr))
+
+    n1 = int(input("Ingresar un numero"))
+    n2 = int(input("Ingresar otro numero"))
+    n3 = int(input("Ingresar otro numero"))
+    n4 = int(input("Ingresar otro numero"))
+    R= n1+n2+n3+n4
+    print("El resultado es: ", R)
+#eje2()
+
+def eje3():
+    lado1 = int(input("Ingresar un numero"))
+    lado2 = int(input("ingresar otro numero"))
+    R= lado1 + lado2
+    print("La superficie es: ", R)
+    #eje3()
+
+def eje4():
+    lados = int(input("Ingresar un numero"))
+    print("La superficie es: ", lados)
+    #eje4
